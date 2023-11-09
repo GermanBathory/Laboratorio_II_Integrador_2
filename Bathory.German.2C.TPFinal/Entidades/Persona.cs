95857a0d4665b9e875f4c849c95f0ded1826be32
GermanBathory <germanbathory@gmail.com>
2023-11-09 09:35:08 -0300

    Agregar archivos de proyecto.

---
+++ Bathory.German.2C.TPFinal/Entidades/Persona.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entidades
+{
+    public abstract class Persona
+    {
+        private string? nombre;
+        private string? apellido;
+
+        public Persona()
+        {
+        }
+
+        public Persona(string nombre, string apellido)
+        {
+            Nombre = nombre;
+            this.apellido = apellido;
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                if (Validadora.ValidarString(value))
+                {
+                    nombre = value;
+                }
+                else
+                {
+                    throw new Exception("El nombre es invalido");
+                }
+
+            }
+        }
+    }
+}
---
+++ Bathory.German.2C.TPFinal/Entidades/Validadora.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Validadora
+    {
+        public static bool ValidarString(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!char.IsLetter(str[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool ValidarIntConMaxYMin(string numero, int min, int max)
+        {
+            if (int.TryParse(numero, out int numeroEntero))
+            {
+                if (numeroEntero > min && numeroEntero < max)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool ValidarFloatConMaxYMin(string numero, int min, int max)
+        {
+            if (float.TryParse(numero, out float numeroFloat))
+            {
+                if (numeroFloat > min && numeroFloat < max)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
